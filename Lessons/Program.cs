a67aa5267bdbdb42c48079f5aa84467eba8c173b
Kiririn <ya.aleks19982@yandex.ru>
2021-01-23 14:00:36 +0300

    picture in line

--- Lessons/Program.cs
+++ Lessons/Program.cs
@@ -10,25 +10,14 @@ namespace Lessons
     {
         static void Main(string[] args)
         {
-            string firstName = "";
-            string lastName = "";
-            string zodiacSign = "";
-            int age = 0;
-            string placeOfWork = "";
+            int picture = 52;
+            int pictureStack = 3;
+            int pictureLine = 0;
+            int pictureOutOfLine = 0;
+            pictureLine = picture / pictureStack;
+            pictureOutOfLine = picture % pictureStack;
+            Console.WriteLine("Из 52 картинок можно собрать " + pictureLine +" рядов по 3 картинки в ряду,после чего останется "+pictureOutOfLine + " картинка");
 
-            Console.Write("Добрый день, введите ваше имя: ");
-            firstName = Console.ReadLine();
-            Console.Write("Введите вашу фамилию: ");
-            lastName = Console.ReadLine();
-            Console.Write("Количество полных лет: ");
-            age = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Ваш знак зодиака: ");
-            zodiacSign = Console.ReadLine();
-            Console.Write("Место Вашей работы: ");
-            placeOfWork = Console.ReadLine();
-
-            Console.WriteLine("Вас зовут "+firstName+" "+lastName+", Вам "+age+" лет, Ваш знак зодиака "+ zodiacSign+" и вы работаете на "+ placeOfWork);
-            Console.ReadKey();
         }
     }
 }

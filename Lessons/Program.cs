f1ac7345edd717f48b92c1dad2abca5db4cc2dd2
Kiririn <ya.aleks19982@yandex.ru>
2021-01-23 13:30:34 +0300

    person card

--- Lessons/Program.cs
+++ Lessons/Program.cs
@@ -10,17 +10,25 @@ namespace Lessons
     {
         static void Main(string[] args)
         {
-            int grannyCount = 0;
-            int waitingHours = 0;
-            int waitingMinute = 0;
-            int receptionTime = 10;
+            string firstName = "";
+            string lastName = "";
+            string zodiacSign = "";
+            int age = 0;
+            string placeOfWork = "";
 
-            Console.Write("Добрый день,введите количество старушек ");
-            grannyCount = Convert.ToInt32(Console.ReadLine());
-            waitingHours = (grannyCount * receptionTime) / 60;
-            waitingMinute = (grannyCount * receptionTime) % 60;
+            Console.Write("Добрый день, введите ваше имя: ");
+            firstName = Console.ReadLine();
+            Console.Write("Введите вашу фамилию: ");
+            lastName = Console.ReadLine();
+            Console.Write("Количество полных лет: ");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Ваш знак зодиака: ");
+            zodiacSign = Console.ReadLine();
+            Console.Write("Место Вашей работы: ");
+            placeOfWork = Console.ReadLine();
 
-            Console.WriteLine("Вы простоите в очереди "+waitingHours +" часа(-ов) и " + waitingMinute+" минут");
+            Console.WriteLine("Вас зовут "+firstName+" "+lastName+", Вам "+age+" лет, Ваш знак зодиака "+ zodiacSign+" и вы работаете на "+ placeOfWork);
+            Console.ReadKey();
         }
     }
 }

0c155eccf1d4dba8f1fa0bed1dcb29f9b64ff43e
Kiririn <ya.aleks19982@yandex.ru>
2021-01-26 16:14:42 +0300

    работаем

--- Lessons/Program.cs
+++ Lessons/Program.cs
@@ -10,14 +10,63 @@ namespace Lessons
     {
         static void Main(string[] args)
         {
-            int picture = 52;
-            int pictureStack = 3;
-            int pictureLine = 0;
-            int pictureOutOfLine = 0;
-            pictureLine = picture / pictureStack;
-            pictureOutOfLine = picture % pictureStack;
-            Console.WriteLine("Из 52 картинок можно собрать " + pictureLine +" рядов по 3 картинки в ряду,после чего останется "+pictureOutOfLine + " картинка");
-
+            string userChoise = "";
+            string userPassword = "";
+            bool userOutput = true;
+                Console.WriteLine("Добрый День,вы находитесь в главном меню,чтобы: ");
+                Console.WriteLine("Чтобы устаноить пароль введите - setpasswd");
+                Console.WriteLine("Чтобы изменить цвет консоли введите - changecolor");
+                Console.WriteLine("Чтобы вывести список команд введите  - help");
+                Console.WriteLine("Чтобы выйти из программы введите - exit");
+                Console.WriteLine("Чтобы узнать дату введите  - time");
+            while (userOutput)
+            {
+                userChoise = Console.ReadLine();
+                switch (userChoise)
+                {
+                    case ("setpasswd"):
+                        Console.Clear();
+                        Console.Write("Введите новый пароль: ");
+                        userPassword = Console.ReadLine();
+                        Console.WriteLine("Новый пароль: " + userPassword);
+                        break;
+                    case ("changecolor"):
+                        Console.Clear();
+                        Console.Write("Чтобы изменить цвет консоли введите пароль: ");
+                        string userInput = Console.ReadLine();
+                        if (userInput !=userPassword ||userPassword == "")
+                        {
+                            Console.WriteLine("Пароль не верный,либо пароль не установлен");
+                        }
+                        else
+                        {
+                            Console.BackgroundColor = ConsoleColor.Red;
+                            Console.ForegroundColor = ConsoleColor.Blue;
+                        }
+                            break;
+                    case ("help"):
+                        Console.Clear();
+                        Console.WriteLine("Чтобы устаноить пароль,введите setpasswd");
+                        Console.WriteLine("Чтобы изменить цвет консоли введите - changecolor");
+                        Console.WriteLine("Чтобы вывести список команд введите  - help");
+                        Console.WriteLine("Чтобы выйти из программы введите - exit");
+                        break;
+                    case ("exit"):
+                        Console.Clear();
+                        Console.WriteLine("Вы уверены,что хотите выйти из программы - Y/N");
+                        string userAnswer = Console.ReadLine();
+                        if (userAnswer =="y" ||userAnswer =="Y")
+                        {
+                            userOutput = false;
+                        }
+                        break;
+                    case ("time"):
+                        Console.Clear();
+                        Console.WriteLine("Сегодня: " + DateTime.Today);
+                        break;
+                }
+            }
+            Console.WriteLine("Всего ХО-РО-ШЕ-ГО");
         }
     }
 }

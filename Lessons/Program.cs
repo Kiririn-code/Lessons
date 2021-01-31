ddbf1693e4db0a0abf541cd04cada65e07715009
Kiririn <ya.aleks19982@yandex.ru>
2021-01-31 15:17:42 +0300

    fix

--- Lessons/Program.cs
+++ Lessons/Program.cs
@@ -12,16 +12,16 @@ namespace Lessons
         {
             int userGold;
             int userCrystal = 0;
-            int crystalRate = 30;
+            int crystalPrice = 30;
             bool isChangePossible;
             Console.Write("Введите количество золота: ");
             userGold = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Добрый день,курс золота к кристаллам =  "+crystalRate+ "\nСколько хотите приобрести? ");
+            Console.Write("Добрый день,курс золота к кристаллам =  "+crystalPrice+ "\nСколько хотите приобрести? ");
             userCrystal = Convert.ToInt32(Console.ReadLine());
-            isChangePossible = userCrystal * crystalRate <= userGold;
+            isChangePossible = userCrystal * crystalPrice <= userGold;
             int changeFactor = Convert.ToInt32(isChangePossible);
             userCrystal *= changeFactor;
-            userGold -= crystalRate * userCrystal * changeFactor;
+            userGold -= crystalPrice * userCrystal;
             Console.WriteLine("У вас - " + userCrystal + " кристалов и "+ userGold + " золота");
         }
     }

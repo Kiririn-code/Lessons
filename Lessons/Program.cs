8b5fc4cc5906bd7911bb9b104d66ca4cc4ce5457
Kiririn <ya.aleks19982@yandex.ru>
2021-02-07 15:50:02 +0300

    clean

--- Lessons/Program.cs
+++ Lessons/Program.cs
@@ -10,27 +10,7 @@ namespace Lessons
     {
         static void Main(string[] args)
         {
-            int[] array = { 1, 2, 3, 4, 5, 6, 7, 8 };
-            Shuffle(array);
-
-            for (int i = 0; i < array.Length; i++)
-            {
-                Console.WriteLine(array[i]);
-            }
-        }
-        static void Shuffle(int[] array)
-        {
-            Random random = new Random(); 
-
-            for (int i = array.Length - 1; i >= 1; i--)
-            {
-                int tempVar;
-                int tempRandom;
-                tempRandom = random.Next(i);
-                tempVar = array[tempRandom];
-                array[tempRandom] = array[i];
-                array[i] = tempVar;
-            }
+            
         }
     }
 }

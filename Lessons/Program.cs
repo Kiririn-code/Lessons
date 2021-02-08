d4cf15eb164d189fce30c891eb09ced8986b154e
Kiririn <ya.aleks19982@yandex.ru>
2021-02-08 11:46:18 +0300

    game

--- Lessons/Program.cs
+++ Lessons/Program.cs
@@ -12,55 +12,18 @@ namespace Lessons
         static void Main(string[] args)
         {
             bool isGameRun = true;
-            int playerX, playerY;
+            int playerX = 0, playerY = 0;
             int playerDX = 0, playerDY = 0;
-            char[,] map = ReadMap("map.txt",out playerX, out playerY);
+            char[,] map = ReadMap("map.txt", ref playerX, ref playerY);
             Console.CursorVisible = false;
             DrawMap(map);
 
             while (isGameRun)
             {
-                Console.SetCursorPosition(playerY, playerX);
-                Console.Write('&');
-
-                if (Console.KeyAvailable)
-                {
-                    ConsoleKeyInfo key = Console.ReadKey(true);
-                    switch (key.Key)
-                    {
-                        case ConsoleKey.W:
-                            playerDX = -1;
-                            playerDY = 0;
-                            break;
-                        case ConsoleKey.A:
-                            playerDX = 0;
-                            playerDY = -1;
-                            break;
-                        case ConsoleKey.D:
-                            playerDX = 0;
-                            playerDY = 1;
-                            break;
-                        case ConsoleKey.S:
-                            playerDX = 1;
-                            playerDY = 0;
-                            break;
-                    }
-
-                    if (map[playerDX + playerX, playerDY + playerY] != '0')
-                    {
-                        Console.SetCursorPosition(playerY, playerX);
-                        Console.Write(" ");
-
-                        playerX += playerDX;
-                        playerY += playerDY;
-
-                        Console.SetCursorPosition(playerY, playerX);
-                        Console.Write('&');
-
-                    }
-                }
+                MovePlayer(ref playerX, ref playerY, ref playerDX, ref playerDY, ref map);
             }
         }
+
         static void DrawMap(char[,] map)
         {
             for (int i = 0; i < map.GetLength(0); i++)
@@ -72,10 +35,9 @@ namespace Lessons
                 Console.WriteLine();
             }
         }
-        static char[,] ReadMap(string mapName, out int playerX, out int playerY)
+
+        static char[,] ReadMap(string mapName, ref int playerX, ref int playerY)
         {
-            playerY = 0;
-            playerX = 0;
             string[] newFile = File.ReadAllLines(mapName);
             char[,] map = new char[newFile.Length, newFile[0].Length];
 
@@ -85,7 +47,7 @@ namespace Lessons
                 {
                     map[i, j] = newFile[i][j];
 
-                    if(map[i,j] =='&')
+                    if (map[i, j] == '&')
                     {
                         playerX = i;
                         playerY = j;
@@ -93,6 +55,45 @@ namespace Lessons
                 }
             }
             return map;
+        }
+
+        static void MovePlayer(ref int playerX, ref int playerY, ref int playerDX, ref int playerDY, ref char[,] map)
+        {
+            if (Console.KeyAvailable)
+            {
+                ConsoleKeyInfo key = Console.ReadKey(true);
+                switch (key.Key)
+                {
+                    case ConsoleKey.W:
+                        playerDX = -1;
+                        playerDY = 0;
+                        break;
+                    case ConsoleKey.A:
+                        playerDX = 0;
+                        playerDY = -1;
+                        break;
+                    case ConsoleKey.D:
+                        playerDX = 0;
+                        playerDY = 1;
+                        break;
+                    case ConsoleKey.S:
+                        playerDX = 1;
+                        playerDY = 0;
+                        break;
+                }
+                if (map[playerDX + playerX, playerDY + playerY] != '0')
+                {
+                    Console.SetCursorPosition(playerY, playerX);
+                    Console.Write(" ");
+
+                    playerX += playerDX;
+                    playerY += playerDY;
+
+                    Console.SetCursorPosition(playerY, playerX);
+                    Console.Write('&');
+
+                }
+            }
         }
     }
 }

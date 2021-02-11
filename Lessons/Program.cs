a130b9cd6118e5c2b09fc206d3147a16fbf8fc9d
Kiririn <ya.aleks19982@yandex.ru>
2021-02-11 17:41:07 +0300

    Dictionary

--- Lessons/Program.cs
+++ Lessons/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.IO;
 
 namespace Lessons
 {
@@ -11,89 +10,30 @@ namespace Lessons
     {
         static void Main(string[] args)
         {
-            bool isGameRun = true;
-            int playerX = 0, playerY = 0;
-            int playerDX = 0, playerDY = 0;
-            char[,] map = ReadMap("map.txt", ref playerX, ref playerY);
-            Console.CursorVisible = false;
-            DrawMap(map);
+            bool isProgramRun = true;
+            string userWord;
 
-            while (isGameRun)
+            Dictionary<string, string> vocabulary = new Dictionary<string, string>();
+            vocabulary.Add("падера", "Сильный ветер с дождем или снегом");
+            vocabulary.Add("цикада", "Прыгающие насекомые с выпуклыми глазами");
+            vocabulary.Add("сабо", "Деревяннй башмак");
+            vocabulary.Add("каган", "титул главы государства у древних тюркских народов");
+
+
+            while (isProgramRun)
             {
-                MovePlayer(ref playerX, ref playerY, ref playerDX, ref playerDY, ref map);
+                Console.WriteLine("Введите слово для проверки: ");
+                userWord = Console.ReadLine().ToLower();
+
+                if (vocabulary.ContainsKey(userWord))
+                    Console.WriteLine($"{userWord} - это {vocabulary[userWord]}");
+                else
+                    Console.WriteLine($"Cлово {userWord} отсутствует в нашем толковом словаре");
+
+                Console.ReadKey();
+                Console.Clear();
             }
         }
 
-        static void DrawMap(char[,] map)
-        {
-            for (int i = 0; i < map.GetLength(0); i++)
-            {
-                for (int j = 0; j < map.GetLength(1); j++)
-                {
-                    Console.Write(map[i, j]);
-                }
-                Console.WriteLine();
-            }
-        }
-
-        static char[,] ReadMap(string mapName, ref int playerX, ref int playerY)
-        {
-            string[] newFile = File.ReadAllLines(mapName);
-            char[,] map = new char[newFile.Length, newFile[0].Length];
-
-            for (int i = 0; i < map.GetLength(0); i++)
-            {
-                for (int j = 0; j < map.GetLength(1); j++)
-                {
-                    map[i, j] = newFile[i][j];
-
-                    if (map[i, j] == '&')
-                    {
-                        playerX = i;
-                        playerY = j;
-                    }
-                }
-            }
-            return map;
-        }
-
-        static void MovePlayer(ref int playerX, ref int playerY, ref int playerDX, ref int playerDY, ref char[,] map)
-        {
-            if (Console.KeyAvailable)
-            {
-                ConsoleKeyInfo key = Console.ReadKey(true);
-                switch (key.Key)
-                {
-                    case ConsoleKey.W:
-                        playerDX = -1;
-                        playerDY = 0;
-                        break;
-                    case ConsoleKey.A:
-                        playerDX = 0;
-                        playerDY = -1;
-                        break;
-                    case ConsoleKey.D:
-                        playerDX = 0;
-                        playerDY = 1;
-                        break;
-                    case ConsoleKey.S:
-                        playerDX = 1;
-                        playerDY = 0;
-                        break;
-                }
-                if (map[playerDX + playerX, playerDY + playerY] != '0')
-                {
-                    Console.SetCursorPosition(playerY, playerX);
-                    Console.Write(" ");
-
-                    playerX += playerDX;
-                    playerY += playerDY;
-
-                    Console.SetCursorPosition(playerY, playerX);
-                    Console.Write('&');
-
-                }
-            }
-        }
     }
 }

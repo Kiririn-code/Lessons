5c2e350372b3ddb7cd5bbff2f1f3b0afbe99c5d5
Kiririn <ya.aleks19982@yandex.ru>
2021-02-11 18:05:34 +0300

    Sum List

--- Lessons/Program.cs
+++ Lessons/Program.cs
@@ -10,7 +10,48 @@ namespace Lessons
     {
         static void Main(string[] args)
         {
-            
+            string userAction;
+            bool isProgramRun = true;
+            List<int> intSum = new List<int>();
+
+            Console.WriteLine("Список комманд:");
+            Console.WriteLine("sum - вывести сумму элементов ранне введенного массива");
+            Console.WriteLine("exit - выйти из программы");
+            while (isProgramRun)
+            {
+                Console.Write("Введите команду: ");
+                userAction = Console.ReadLine();
+                switch(userAction)
+                {
+                    case "exit":
+                        isProgramRun = false;
+                        break;
+                    case "sum":
+                        SumList(intSum);
+                        break;
+                    default:
+                        try
+                        {
+                            int newElement = int.Parse(userAction);
+                            intSum.Add(newElement);
+                        }
+                        catch(Exception)
+                        {
+                            Console.WriteLine("Вы ввели некорретктное значение");
+                        }
+                        break;
+                }
+            }
+        }
+
+        static void SumList (List<int> intSum)
+        {
+            int sum = 0;
+            foreach (var item in intSum)
+            {
+                sum += item;
+            }
+            Console.WriteLine(sum);
         }
     }
 }

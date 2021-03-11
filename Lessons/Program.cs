bff109b481b10e2eb5ffd4026899fff003a47e94
Kiririn <ya.aleks19982@yandex.ru>
2021-03-11 11:30:16 +0300

    Tabula Rasa

--- Lessons/Program.cs
+++ Lessons/Program.cs
@@ -10,38 +10,7 @@ namespace Lessons
     {
         static void Main(string[] args)
         {
-            bool isProgramRun = true;
-            string userWord;
-
-            Dictionary<string, string> vocabulary = new Dictionary<string, string>();
-            vocabulary.Add("падера", "Сильный ветер с дождем или снегом");
-            vocabulary.Add("цикада", "Прыгающие насекомые с выпуклыми глазами");
-            vocabulary.Add("сабо", "Деревяннй башмак");
-            vocabulary.Add("каган", "титул главы государства у древних тюркских народов");
-
-
-            while (isProgramRun)
-            {
-                Console.WriteLine("Чтобы выйти из программы введите - exit");
-                Console.WriteLine("Введите слово для проверки: ");
-                userWord = Console.ReadLine().ToLower();
-                switch (userWord)
-                {
-                    case "exit":
-                        isProgramRun = false;
-                        Console.WriteLine("Завершение работы");
-                        break;
-                    default:
-                        if (vocabulary.ContainsKey(userWord))
-                            Console.WriteLine($"{userWord} - это {vocabulary[userWord]}");
-                        else
-                            Console.WriteLine($"Cлово {userWord} отсутствует в нашем толковом словаре");
-                        break;
-                }
-                Console.ReadKey();
-                Console.Clear();
-            }
+           
         }
-
     }
 }
